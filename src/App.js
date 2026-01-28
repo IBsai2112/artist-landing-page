215010c0e7f6bc92b1c7cc3c68e6a91587b4741c
Sainjal Kalnekar <sainjalkalnekar21@gmail.com>
2026-01-28 10:49:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <div id="nav">
+        <img src="/circle.png" alt="logo" />
+        <h5>Kalyani Priyadarshan</h5>
+      </div>
+
+      <div id="part1"></div>
+
+      <div id="part2">
+        <div id="overlay"></div>
+      </div>
+
+      <div id="part3"></div>
+
+      <div id="text-div">
+        <h1>Nature</h1>
+        <h1>Morte</h1>
+        <h1>For</h1>
+        <h1>Harper&apos;s</h1>
+      </div>
+
+      <div id="smallImageDiv"></div>
+
+      <div id="verticalText">
+        <h1>selection winter</h1>
+      </div>
     </div>
   );
 }
